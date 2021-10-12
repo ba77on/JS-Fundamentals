cc70131c287525149e801597c12dfd470aa9f27b
ba77on <78099942+ba77on@users.noreply.github.com>
2021-10-12 21:51:12 +0300

    Update task6bombNums.js

--- ArraysAdvancedExercise/task6bombNums.js
+++ ArraysAdvancedExercise/task6bombNums.js
@@ -1,30 +1,4 @@
-// function bombNums(arr, bombInfo) {
-//     let bomb = bombInfo[0];
-//     let range = bombInfo[1];
 
-//     let index = arr.indexOf(bomb);
-
-//     while (index > -1) {
-//         arr.splice(Math.max((index - range), 0), Math.min(range, index))
-//         index = arr.indexOf(bomb);
-//         arr.splice(index, (range - 1))
-
-//         index = arr.indexOf(bomb);
-//     }
-//     console.log(calculateSum(arr))
-
-//     function calculateSum(array) {
-//         let sum = 0;
-
-//         for (let n of array) {
-//             sum += n;
-//         }
-
-//         return sum;
-//     }
-
-
-// }
 
 function bomb(numbers, params) {
     let [target, power] = params;
