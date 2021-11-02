b163594d11fc7ea2234106d4ce6614414651f83e
ba77on <78099942+ba77on@users.noreply.github.com>
2021-11-02 10:14:17 +0200

    Update addTaskForFun.js

--- Object and Classes/addTaskForFun.js
+++ Object and Classes/addTaskForFun.js
@@ -1,39 +1,4 @@
-// // let cat = { name: 'Timmy', age: 3, breed: 'Persian' };
-// // let cat2 = { name: 'Gary', age: 2, breed: 'Angora' };
-// // let cat = {
-// //     cat['name'] = 'Nav';
-// //     cat.age = 5;
-// //     cat.breed = 'Persian';
 
-// // function makeCatSound() {
-
-// //     console.log('Meow')
-// // }
-
-// let cat = {
-//     name: 'Navcho',
-//     age: 5,
-//     // makesound: makeCatSound() -- 1 nachin
-
-//     // makeSound: () => {
-//     //     console.log('Meow'); - 2 ri nachin za izvikvane
-//     // }
-
-//     breed: 'Persian',
-
-//     // Поставя се задължителна запетайка след всяка функция!!!
-// }
-
-// for (let prop in cat) {
-//     console.log(cat[prop])
-// }
-// // let catProperties = Object.keys(cat);
-// // let catValues = Object.values(cat);
-// // let catEntries = Object.entries(cat);
-
-// // console.log(catProperties);
-// // console.log(catValues);
-// // console.log(catEntries);
 
 let games = {
     firstGame: 'CounterStrike',
