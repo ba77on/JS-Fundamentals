36ecf8e7643fe8fbe22c80dd6e8466077b3a8a36
ba77on <78099942+ba77on@users.noreply.github.com>
2022-01-19 22:33:05 +0200

    Add files via upload

---
+++ Text Processing/task3.js
@@ -0,0 +1,18 @@
+function solve(text, word) {
+    let start = text.indexOf(word);
+
+    while (start != -1) {
+        let first = text.substring(0, start);
+
+        let second = text.substring(start + word.length);
+
+        text = first + '*'.repeat(word.length) + second;
+
+        start = text.indexOf(word)
+    }
+
+    console.log(text)
+
+}
+solve('A small sentence with some words',
+    'small')
